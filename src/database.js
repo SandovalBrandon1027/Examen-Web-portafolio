839e7a822ec22c650daab96b86cafda0cdda7c63
Jhordy1-1 <150082943+Jhordy1-1@users.noreply.github.com>
2023-12-28 21:14:11 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(MONGODB_URI, {
+        await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
